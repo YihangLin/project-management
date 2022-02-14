2e98299d610e64470f9bb8dfe6a397c1060a0775
YihangLin <yihanglin0529@gmail.com>
2022-02-14 16:40:25 -0400

    setup routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import Navbar from './components/Navbar';
 import './scss/App.scss';
 
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Completed from './pages/Completed';
+import Design from './pages/Design';
+import Development from './pages/Development';
+import Marketing from './pages/Marketing';
+import MyTasks from './pages/MyTasks';
+import InProgress from './pages/Inprogress';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div className='main'>
-        
-        <div className='test'>
-          <h1>test</h1>
-        </div>
 
+      <BrowserRouter>
+      <Navbar />
+
+      <div className='main'>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/login' element={ <Login /> } />
+          <Route path='/signup' element={ <Signup /> } />
+          <Route path='/completed' element={ <Completed /> } />
+          <Route path='/design' element={ <Design /> } />
+          <Route path='/development' element={ <Development /> } />
+          <Route path='/marketing' element={ <Marketing /> } />
+          <Route path='/mytasks' element={ <MyTasks /> } />
+          <Route path='/inprogress' element={ <InProgress /> } />
+        </Routes>
       </div>
+      </BrowserRouter>
     </div>
   );
 }
